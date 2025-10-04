0d5acd51588f3ea0d1d37adc750c7a5f25e3282a
Manikandan-Rangarajan <username270904@gmail.com>
2025-10-04 16:12:37 +0530

    Handiling url fixing in frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5432/api", // change this if your backend URL is different
+  baseURL: "http://localhost:5000/api", // change this if your backend URL is different
 });
 
 export default API;
